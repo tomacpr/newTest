d4828cc6318d8d9f9430fa7c5e30cd6220e5ab8e
Your Name <tomacpr@gmail.com>
2021-12-26 17:48:17 +0200

    hello commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Amigo");
+        Person person = new Person("Jamila");
+        System.out.println(person.getName());
+
+    }
+}
